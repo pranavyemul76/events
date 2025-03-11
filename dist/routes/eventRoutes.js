d70687bbe39f3413793a4da4b8285c730fb71949
pranavyemul76 <pranavyemul76@gmail.com>
2025-03-11 18:08:22 +0530

    add dist

--- dist/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createEvent, getAllEvents } from "../controllers/eventController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
-import { adminAuth } from "../middleware/adminAuth.js";
-const router = express.Router();
-router.post("/post", authMiddleware, adminAuth, createEvent);
-router.get("/get", getAllEvents);
-export default router;
