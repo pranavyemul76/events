ace8cc86dba04a748fe5cdb957ea7fb57fe90a43
pranavyemul76 <pranavyemul76@gmail.com>
2025-03-11 17:53:09 +0530

    remove build file

--- dist/models/eventModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const eventSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    date: { type: Date, required: true },
-    location: { type: String, required: true },
-    organizer: { type: Schema.Types.ObjectId, ref: "User" },
-});
-export default mongoose.model("Event", eventSchema);
