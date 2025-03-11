2ab794a2f325c99cf6d9167906631d67bec10768
pranavyemul76 <pranavyemul76@gmail.com>
2025-03-11 18:20:10 +0530

    frontend

--- build/app.js
+++ build/app.js
@@ -13,16 +13,18 @@ const app = express();
 const port = Number(process.env.PORT);
 const host = process.env.VERCEL_URL?.toString() || "";
 connectDB();
-app.use(cors({
+app.use(
+  cors({
     origin: "*", // Specify the allowed origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-}));
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
-app.use(express.static(path.join(__dirname, "builds")));
+app.use(express.static(path.join(__dirname, "build")));
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "builds", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(port, host, () => console.log(`Server running on port ${port}`));
